b4ed701323ed1f3c3cc47192a7ffcd881c7f224a
Muver1337 <muvers1s@gmail.com>
2023-03-17 13:14:15 +0700

    Добавил создание заметки и начал создание таблицы из 4 колонн

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ let eventBus = new Vue()
 
 Vue.component('allcolumn', {
     template: `
-
-<div id="cols"
-    <column1></column1>
-</div>
+<div class="cols">
+            <create></create>
+            <div class="tabs-back">
+            </div>   
+        </div>
     `,
     data() {
         return {
-            column1: [],
+            column_1:[],
+            column_2:[],
+            column_3:[],
+            column_4:[],
             errors: [],
         }
     },
 })
 
-Vue.component('column1', {
+Vue.component('column_1', {
     template: `
     <div class="cols-wrapper">
             <div class="col">
@@ -38,6 +42,66 @@ Vue.component('column1', {
     },
 })
 
+Vue.component('create', {
+    template: `
+<slot name="body">
+    <div class="createf">
+        <form class="create" @submit.prevent="onSubmit">
+            <label for="title">Заголовок</label>
+            <input id="title" v-model="title" type="text" placeholder="Заголовок" required maxlength="30">   
+            <label for="description">Описание</label>
+            <textarea id="description" v-model="description" rows="5" columns="10" required maxlength="60"></textarea>
+            <label for="deadline">Дедлайн</label>
+            <input id="deadline" type="date" v-model="deadline" placeholder="дд.мм.гггг" required>        
+            <label for="priority">Приоретет:</label>
+            <select id="priority" v-model.number="priori">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+            </select>
+            <button type="submit">Создать</button>
+        </form>
+    </div>
+</slot>  
+    `,
+    data() {
+        return {
+            title: null,
+            description: null,
+            date: null,
+            deadline: null,
+            show: false,
+            reason: [],
+            priori: null,
+        }
+    },
+    methods: {
+        onSubmit() {
+            let tab = {
+                title: this.title,
+                description: this.description,
+                date: new Date().toLocaleDateString().split('.').reverse().join('-'),
+                deadline: this.deadline,
+                edit: null,
+                editButton: false,
+                refund: false,
+                term: true,
+                reason: [],
+                priori: this.priori
+            }
+            eventBus.$emit('addColumn_1', tab);
+            this.title = null;
+            this.description = null;
+            this.date = null;
+            this.deadline = null;
+            this.show = false;
+            this.priori = null;
+        },
+        close() {
+            this.show = false;
+        }
+    }
+})
 
 let app = new Vue({
     el: '#app',
